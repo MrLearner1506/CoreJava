d6b6f48961d3ce42bc2f23ef28d3fa8691f8d59a
naren <iyneshwar15@gmail.com>
2025-05-01 12:11:04 +0530

    Datatype programes

---
+++ Datatypes.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Datatypes
+{
+	static Scanner scan=new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+		System.out.print("Id : ");
+		int id=scan.nextInt();
+		
+		System.out.print("Grade : ");
+		scan.nextLine();
+		 char grade=scan.nextLine().charAt(0);
+	
+	}
+}
